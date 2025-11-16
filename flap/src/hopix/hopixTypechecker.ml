b34eb91cb962e0e053447afb997f0e200975ff1a
Amenah <amenah.mushtaq@etu.u-paris.fr>
2025-11-16 14:32:01 +0100

    debut typage

--- flap/src/hopix/hopixTypechecker.ml
+++ flap/src/hopix/hopixTypechecker.ml
@@ -22,62 +22,111 @@ let check_equal_types pos ~expected ~given =
 
 (** Linearity-checking code for patterns *)
 
+(* MOI *)
 let rec check_pattern_linearity
         : identifier list -> pattern Position.located -> identifier list
   = fun vars Position.{ value; position; } ->
-  failwith "Students! This is your job!"
-
+    match value with
+      | PVariable id -> failwith "PVAR"
+      | PWildcard -> failwith "WILDCARD"
+      | PTypeAnnotation(pat,t) -> failwith "P TYPE ANO"
+      | PLiteral(l) -> failwith "PLit"
+      | PTaggedValue(c,typeList,p) -> failwith "PTaggedVal"
+      | PRecord(l,tl) -> failwith "PRecord"
+      | PTuple(l) -> failwith "PTuple"
+      | POr(l) -> failwith "POR"
+      | PAnd(l) -> failwith "Pand"
+      
 (** Type-checking code *)
-
+(* renvoie un env *)
 let check_type_scheme :
       HopixTypes.typing_environment ->
       Position.t ->
       HopixAST.type_scheme ->
       HopixTypes.aty_scheme * HopixTypes.typing_environment
   = fun env pos (ForallTy (ts, ty)) ->
-  failwith "Students! This is your job!"
+  failwith "Jules"
 
+  (* MOI *)
 let synth_literal : HopixAST.literal -> HopixTypes.aty =
   fun l ->
-  failwith "Students! This is your job!"
-
+    match  l with
+    | LInt _ -> HopixTypes.hint
+    | LString _ -> HopixTypes.hstring
+    | LChar _ -> HopixTypes.hchar
+    
 let rec check_pattern :
           HopixTypes.typing_environment ->
           HopixAST.pattern Position.located ->
           HopixTypes.aty ->
           HopixTypes.typing_environment
   = fun env Position.({ value = p; position = pos; } as pat) expected ->
-  failwith "Students! This is your job!"
+    failwith "Jules"
 
+  (* MOI *)
 and synth_pattern :
       HopixTypes.typing_environment ->
       HopixAST.pattern Position.located ->
       HopixTypes.aty * HopixTypes.typing_environment
   = fun env Position.{ value = p; position = pos; } ->
-  failwith "Students! This is your job!"
-
+  match p with
+    | PVariable id -> failwith "PVAR"
+    | PWildcard -> failwith "WILDCARD"
+    | PTypeAnnotation(pat,t) -> failwith "P TYPE ANO"
+    | PLiteral(l) -> failwith "PLit"
+    | PTaggedValue(c,typeList,p) -> failwith "PTaggedVal"
+    | PRecord(l,tl) -> failwith "PRecord"
+    | PTuple(l) -> failwith "PTuple"
+    | POr(l) -> failwith "POR"
+    | PAnd(l) -> failwith "Pand"
+    
 let rec synth_expression :
       HopixTypes.typing_environment ->
       HopixAST.expression Position.located ->
       HopixTypes.aty
   = fun env Position.{ value = e; position = pos; } ->
-  failwith "Students! This is your job!"
+    failwith "Jules"
 
+  (* MOI *)
 and check_expression :
       HopixTypes.typing_environment ->
       HopixAST.expression Position.located ->
       HopixTypes.aty ->
       unit
   = fun env (Position.{ value = e; position = pos; } as exp) expected ->
-  failwith "Students! This is your job!"
+    match e with 
+    | Literal litLoc -> 
+      let litType = synth_literal (Position.value litLoc) in
+      if litType <> expected then failwith "TODO LIT"
+    | Variable(idLoc,l) ->
+      failwith "VAR"
+    | Tagged(cloc,typelist,exprList) -> failwith "TAGGED"
+    | Record(l,typelist) -> failwith "RECORD"
+    | Field(expr,lab,typelist) -> failwith "FIELD"
+    | Tuple(exprList) -> failwith "TUPLE"
+    | Sequence(exprList) -> failwith "SEQUENCE"
+    | Define(v,expr) -> failwith "DEFINE"
+    | Fun(f) -> failwith "FUN"
+    | Apply(expr1,expr2) -> failwith "APPLY"
+    | Ref(expr) -> failwith "REF"
+    | Assign(expr1,expr2) -> failwith "ASSIGN"
+    | Read(expr) -> failwith "READ"
+    | Case(e,b) -> failwith "CASE"
+    | IfThenElse(e1,e2,e3) -> failwith "if then else"
+    | While(condition,expr) -> failwith "while"
+    | For(id,debutExpr,finExpr,body) -> failwith "FOR"
+    | TypeAnnotation(expr,t) ->
+       failwith "TYPE ANNO"
+  
+  
 
 and check_value_definition :
       HopixTypes.typing_environment ->
       HopixAST.value_definition ->
       HopixTypes.typing_environment
   = fun env def ->
-  failwith "Students! This is your job!"
-
+    failwith "Jules"
+  (* synthèse *)
 let check_definition env = function
   | DefineValue vdef ->
      check_value_definition env vdef
@@ -89,7 +138,7 @@ let check_definition env = function
   | DeclareExtern (x, tys) ->
      let tys, _ = Position.located_pos (check_type_scheme env) tys in
      HopixTypes.bind_value (Position.value x) tys env
-
+(* TODO à completer *)
 let typecheck env program =
   List.fold_left
     (fun env d -> Position.located (check_definition env) d)
