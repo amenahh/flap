f1484c6dd18549a96a1522d213da066a0cacc4be
Amenah <amenah.mushtaq@etu.u-paris.fr>
2025-11-18 18:42:45 +0100

    ref,var,apply

--- flap/src/hopix/hopixTypechecker.ml
+++ flap/src/hopix/hopixTypechecker.ml
@@ -1,7 +1,7 @@
 (** This module implements a bidirectional type checker for Hopix. *)
 
 open HopixAST
-
+open HopixTypes
 (** Error messages *)
 
 let invalid_instantiation pos given expected =
@@ -45,7 +45,7 @@ let check_type_scheme :
       HopixAST.type_scheme ->
       HopixTypes.aty_scheme * HopixTypes.typing_environment
   = fun env pos (ForallTy (ts, ty)) ->
-  failwith "Jules"
+    failwith "AAA"
 
   (* MOI *)
 let synth_literal : HopixAST.literal -> HopixTypes.aty =
@@ -61,7 +61,7 @@ let rec check_pattern :
           HopixTypes.aty ->
           HopixTypes.typing_environment
   = fun env Position.({ value = p; position = pos; } as pat) expected ->
-    failwith "Jules"
+    failwith "todo check_pattern"
 
   (* MOI *)
 and synth_pattern :
@@ -79,13 +79,75 @@ and synth_pattern :
     | PTuple(l) -> failwith "PTuple"
     | POr(l) -> failwith "POR"
     | PAnd(l) -> failwith "Pand"
-    
+
+    (*     
+x avec t_barre comme type scheme se synthéthise en t' ou on remplece alpha par t_barre
+
+SI t_barre est un type bien formé 
+
+ET que la variable x avec comme type pourtout alpha_bare t' est dans l'environement *)
+
 let rec synth_expression :
       HopixTypes.typing_environment ->
       HopixAST.expression Position.located ->
       HopixTypes.aty
   = fun env Position.{ value = e; position = pos; } ->
-    failwith "Jules"
+    match e with
+    | Literal litLoc -> synth_literal (Position.value litLoc)
+    | Variable(idLoc,lopt) ->
+      begin
+      match lopt with
+      | Some l -> 
+        let scheme = List.map (internalize_ty env) l in 
+        let atyScheme = lookup_type_scheme_of_identifier (Position.position idLoc) (Position.value idLoc ) env  in
+       instantiate_type_scheme atyScheme scheme
+      | None ->
+        let atyScheme = lookup_type_scheme_of_identifier (Position.position idLoc) (Position.value idLoc ) env  in
+      instantiate_type_scheme atyScheme []
+      end
+    | Tagged(cloc,typelist,exprList) -> failwith "TAGGED"
+    | Record(l,typelist) -> failwith "RECORD"
+    | Field(expr,lab,typelist) -> failwith "FIELD"
+    | Tuple(exprList) -> failwith "TUPLE"
+    | Sequence(exprList) -> 
+      (* synth_sequence exprList env *)
+      failwith "SEQUENCE"
+    | Define(v,expr) -> failwith "DEFINE"
+    | Fun(f) -> failwith "FUN"
+    | Apply(expr1,expr2) ->
+      let v1 = synth_expression env expr1 in
+      let t1,t2 = destruct_function_type (Position.position expr1) v1 in
+      check_expression env expr2 t1;
+      t2
+    | Ref(expr) -> href (synth_expression env expr)
+      (* failwith "REF" *)
+    | Assign(expr1,expr2) -> 
+      (* let t1 = synth_expression env expr1 in
+      check_expression env expr2 t1;
+      t1 *)
+      failwith "ASSIGN"
+    | Read(expr) -> 
+      synth_expression env expr
+      (* failwith "READ" *)
+    | Case(e,b) -> failwith "CASE"
+    | IfThenElse(e1,e2,e3) -> failwith "if then else"
+    | While(condition,expr) -> failwith "while"
+    | For(id,debutExpr,finExpr,body) ->
+      hunit
+       (* failwith "FOR" *)
+    | TypeAnnotation(expr,t) ->
+        let expr_aty =internalize_ty env t in
+        check_expression env expr expr_aty;
+        expr_aty
+       (* failwith "TYPE ANNO" *)
+
+and synth_sequence exprList env =
+  match exprList with
+  | [] -> failwith "c pas possible"
+  | [e] -> synth_expression env e
+  | expr::l -> 
+    check_expression env expr hunit;
+    synth_sequence l env
 
   (* MOI *)
 and check_expression :
@@ -97,26 +159,47 @@ and check_expression :
     match e with 
     | Literal litLoc -> 
       let litType = synth_literal (Position.value litLoc) in
-      if litType <> expected then failwith "TODO LIT"
-    | Variable(idLoc,l) ->
-      failwith "VAR"
+      check_equal_types pos expected litType;
+    | Variable _ ->
+      let t = synth_expression env exp in
+      check_equal_types pos expected t
     | Tagged(cloc,typelist,exprList) -> failwith "TAGGED"
     | Record(l,typelist) -> failwith "RECORD"
     | Field(expr,lab,typelist) -> failwith "FIELD"
     | Tuple(exprList) -> failwith "TUPLE"
-    | Sequence(exprList) -> failwith "SEQUENCE"
+    | Sequence(exprList) -> 
+      let _ = synth_sequence exprList env in
+      (* failwith "SEQUENCE" *)
+      ()
     | Define(v,expr) -> failwith "DEFINE"
     | Fun(f) -> failwith "FUN"
-    | Apply(expr1,expr2) -> failwith "APPLY"
-    | Ref(expr) -> failwith "REF"
-    | Assign(expr1,expr2) -> failwith "ASSIGN"
-    | Read(expr) -> failwith "READ"
+    | Apply _ -> 
+      let t = synth_expression env exp in 
+      check_equal_types pos expected t;
+    | Ref _ -> 
+      let t = synth_expression env exp in
+      check_equal_types (Position.position exp) expected t
+      (* failwith "REF" *)
+    | Assign(expr1,expr2) -> 
+      let t1 = synth_expression env expr1 in
+      let ty = destruct_reference_type (Position.position expr1) t1 in
+      check_expression env expr2 ty;
+      (* failwith "ASSIGN" *)
+    | Read(expr) -> 
+      let ty = synth_expression env expr in
+      let t = destruct_reference_type (Position.position expr) ty in
+      check_equal_types pos expected t
+      (* failwith "READ" *)
     | Case(e,b) -> failwith "CASE"
     | IfThenElse(e1,e2,e3) -> failwith "if then else"
     | While(condition,expr) -> failwith "while"
     | For(id,debutExpr,finExpr,body) -> failwith "FOR"
     | TypeAnnotation(expr,t) ->
-       failwith "TYPE ANNO"
+      let expr_aty =internalize_ty env t in
+      check_expression env expr expr_aty;
+        
+      (* () *)
+       (* failwith "TYPE ANNO" *)
   
   
 
@@ -125,7 +208,7 @@ and check_value_definition :
       HopixAST.value_definition ->
       HopixTypes.typing_environment
   = fun env def ->
-    failwith "Jules"
+    failwith "TODO"
   (* synthèse *)
 let check_definition env = function
   | DefineValue vdef ->
@@ -138,8 +221,8 @@ let check_definition env = function
   | DeclareExtern (x, tys) ->
      let tys, _ = Position.located_pos (check_type_scheme env) tys in
      HopixTypes.bind_value (Position.value x) tys env
-(* TODO à completer *)
-let typecheck env program =
+
+let typecheck env program = (* TODO à completer *)
   List.fold_left
     (fun env d -> Position.located (check_definition env) d)
     env program
--- flap/src/hopix/hopixTypes.mli
+++ flap/src/hopix/hopixTypes.mli
@@ -11,6 +11,7 @@
 open HopixAST
 
 (** Internal types. *)
+(* C les types sans les positions *)
 type aty =
   | ATyVar   of type_variable
   | ATyCon   of type_constructor * aty list
@@ -29,7 +30,9 @@ val type_error : Position.t -> string -> 'a
 val string_of_aty : aty -> string
 
 val monomorphic_type_scheme : aty -> aty_scheme
-
+(* 
+ça instancie le schéma de type avec une liste de types qu'on connait
+*)
 val instantiate_type_scheme : aty_scheme -> aty list -> aty
 
 (** {3 Type construction and destruction} *)
