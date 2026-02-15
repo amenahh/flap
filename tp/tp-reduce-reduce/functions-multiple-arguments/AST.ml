9c09d9b57ab845cbafa6366c4f55edd5afe0c25c
amenahh <158849158+amenahh@users.noreply.github.com>
2026-02-16 00:34:33 +0100

    Delete tp directory

--- tp/tp-reduce-reduce/functions-multiple-arguments/AST.ml
+++
@@ -1,9 +0,0 @@
-type expression =
-Var of Id.t
-| Int of int
-| Add of expression * expression
-| Fun of boundN
-| App of expression * expression list
-
-and boundN = { bound : Id.t list;
-               body : expression; }
